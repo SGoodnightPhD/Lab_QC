b3f7d7e1989e543c5fab8d05d879e207a75aa80f
SGoodnightPhD <121902621+SGoodnightPhD@users.noreply.github.com>
2024-10-23 22:51:34 -0400

    Update render_report.R
    Now makes a directory named after the input file.

--- render_report.R
+++ render_report.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(ggplot2)
 library(knitr)
 
-RawResults.dat <- read.csv("2024-05-15 Tick Pathogen PCR Test 6(Results).csv", header = FALSE)
+csv_file <- "2024-05-15 Tick Pathogen PCR Test 6(Results).csv"
+
+RawResults.dat <- read.csv(csv_file, header = FALSE)
 PCR_Results.dat <- tail(RawResults.dat, n = -46)
 PCR_Results.dat <- row_to_names(PCR_Results.dat, row_number = 1)
 PCR_Results.dat <- clean_names(PCR_Results.dat)
@@ -12,8 +14,10 @@ PCR_Results.dat$quantity <- as.numeric(PCR_Results.dat$quantity)
 PCR_Results.dat$`r_superscript_2` <- as.numeric(PCR_Results.dat$r_superscript_2)
 PCR_Results.dat$ct <- ifelse(is.na(PCR_Results.dat$ct), 41, PCR_Results.dat$ct)
 Samples <- unique(PCR_Results.dat$sample_name)
+csv_base_name <- tools::file_path_sans_ext(basename(csv_file))
 
 setwd("C:/Users/sethg/OneDrive/Documents/CoCo Lab/Lab_QC")
+dir.create(csv_base_name, showWarnings = FALSE)
 
 # Loop through each sample name and generate a report
 for (sample in Samples) {
@@ -23,5 +27,6 @@ for (sample in Samples) {
   # Render the R Markdown file for each sample
   rmarkdown::render("Tick Report.Rmd", 
                     params = list(report_name = report_name), 
-                    output_file = paste0(report_name, ".pdf"))
+                    output_file = paste0(csv_base_name, "/", report_name, ".pdf"))
 }
+
