1a4f072b57bd6b80dd37cbc10e4d6234f1b804fa
SGoodnightPhD <121902621+SGoodnightPhD@users.noreply.github.com>
2024-10-23 22:29:33 -0400

    Multiple files from a PCR result file
    Created a n R script with a loop that makes an output file named for each sample name in the qPCR result file.

---
+++ render_report.R
@@ -0,0 +1,27 @@
+library(janitor)
+library(tidyverse)
+library(ggplot2)
+library(knitr)
+
+RawResults.dat <- read.csv("2024-05-15 Tick Pathogen PCR Test 6(Results).csv", header = FALSE)
+PCR_Results.dat <- tail(RawResults.dat, n = -46)
+PCR_Results.dat <- row_to_names(PCR_Results.dat, row_number = 1)
+PCR_Results.dat <- clean_names(PCR_Results.dat)
+PCR_Results.dat$ct <- as.numeric(PCR_Results.dat$ct)
+PCR_Results.dat$quantity <- as.numeric(PCR_Results.dat$quantity)
+PCR_Results.dat$`r_superscript_2` <- as.numeric(PCR_Results.dat$r_superscript_2)
+PCR_Results.dat$ct <- ifelse(is.na(PCR_Results.dat$ct), 41, PCR_Results.dat$ct)
+Samples <- unique(PCR_Results.dat$sample_name)
+
+setwd("C:/Users/sethg/OneDrive/Documents/CoCo Lab/Lab_QC")
+
+# Loop through each sample name and generate a report
+for (sample in Samples) {
+  # Define the name of the output dynamically for each sample
+  report_name <- sample
+  
+  # Render the R Markdown file for each sample
+  rmarkdown::render("Tick Report.Rmd", 
+                    params = list(report_name = report_name), 
+                    output_file = paste0(report_name, ".pdf"))
+}
