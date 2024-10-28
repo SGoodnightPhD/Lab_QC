3ca281dc3eedf1b9f79a1c5c1ccd930c6694a71a
SGoodnightPhD <121902621+SGoodnightPhD@users.noreply.github.com>
2024-10-28 14:41:14 -0400

    Updated file paths

--- Tick PCR Results.R
+++ Tick PCR Results.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(ggplot2)
 library(knitr)
 
-csv_file <- "TestPlate.csv"
+csv_file <- "2024-05-15 Tick Pathogen PCR Test 6(Results).csv"
 
 RawResults.dat <- read.csv(csv_file, header = FALSE)
 PCR_Results.dat <- tail(RawResults.dat, n = -46)
@@ -18,17 +18,15 @@ Samples <- unique(PCR_Results.dat$sample_name)
 csv_base_name <- tools::file_path_sans_ext(basename(csv_file))
 
 #setwd("C:/Users/sethg/OneDrive/Documents/CoCo Lab/Lab_QC")
-setwd("C:/Users/Seth/Documents/GitHub/Lab_QC")
+setwd("C:/Users/swd22/OneDrive - USNH/Documents/GitHub/Lab_QC")
 dir.create(csv_base_name, showWarnings = FALSE)
 
 # Loop through each sample name and generate a report
 for (sample in Samples) {
   # Define the name of the output dynamically for each sample
   report_name <- sample
-  
   # Render the R Markdown file for each sample
   rmarkdown::render("Tick Report.Rmd", 
                     params = list(report_name = report_name), 
                     output_file = paste0(csv_base_name, "/", report_name, ".pdf"))
 }
-
